7ebbe1cf8ae1a61560e7bc790899dacd317faaed
lucas del valle <81876236+Lucasdv96@users.noreply.github.com>
2025-09-12 18:18:23 -0300

    Instalacion nueva de Node
    En esta branch estuve trabajando realizando de cero como iniciar una app con node utilizando lo visto en clase

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,7 @@
-const express = require('express');
-const app = express();
+import express from 'express';
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  res.send('Hola Mundo');
-  res.send('Bonjour le monde');
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-  console.log('Servidor ejecutándose en http://localhost:3000');
-});
+export function createApp() {
+    const app = express();
+    app.use(express.json());
+    return app;
+}
